7461c817cdf5c7c35c84d6c68b33544007aece90
Lerato-Lee <mthimkhululerato72@gmail.com>
2022-09-09 08:23:22 -0700

    quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main- prints exactly and that piece of art is useful
  * followed by a new line, to the standard error.
- * Return: Always zero(Success)
+ * Return: Always 1 (Success)
  */
 int main(void)
 {
-	write (2, "and that piece of art is useful\" -Dora Kopar, 2015-10-19\n", 59);
-	return(1); 
+	write (2, "and that piece of art is useful\" - Dora Kopar, 2015-10-19\n", 59);
+	return (1) ; 
 }
