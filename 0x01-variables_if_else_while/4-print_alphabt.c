f0cd2bd8016943c4e202d863ff3c0e57759bfe0b
Lerato-Lee <mthimkhululerato72@gmail.com>
2022-09-10 01:38:45 -0700

    PrintS

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -11,12 +11,11 @@ int main(void)
 char ch;
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-	putchar(ch);
-}
-for (ch = 'A' ; ch <= 'Z' ; ch++)
+if (ch = 'A' ; ch <= 'Z' ; ch++)
 {
 	putchar(ch);
-}
+} else
+{
 putchar('\n');
 return (0);
 }
