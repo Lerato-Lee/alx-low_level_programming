d39eade22b226132eca5bd911db7ed8c306e0ec4
Lerato-Lee <mthimkhululerato72@gmail.com>
2022-09-11 05:44:32 -0700

    print base

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,37 +6,27 @@
  * Return: returns zero at the end
  */
 int main(void)
-
 {
 	int i, j;
 
 	for (i = 0; i <= 9; i++)
 
 	{
-
-		for (j = 1; j <= 9; j++)
+		if (j > i)
 
 		{
-
 			putchar(i + '0');
-
 			putchar(j + '0');
-
 			if (i != 8)
 
 			{
-
 				putchar(',');
-
 				putchar(' ');
 			}
-
 		}
-
 	}
 
 	putchar('\n');
 
-	return(0);
-
+	return (0);
 }
