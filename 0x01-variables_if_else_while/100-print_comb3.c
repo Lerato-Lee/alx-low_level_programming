a0afaef970846232eb5e8bba54050133be2d403e
Lerato-Lee <mthimkhululerato72@gmail.com>
2022-09-11 05:36:15 -0700

    print combo

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,42 @@
+#include<stdio.h>
+
+/**
+ * main - print single digit numbers starting from 0
+ *
+ * Return: Always (Success);
+ */
+int main(void)
+
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+
+	{
+
+		for (j = 1; j <= 9; j++)
+
+		{
+
+			putchar(i + '0');
+
+			putchar(j + '0');
+
+			if (i != 8)
+
+			{
+
+				putchar(',');
+
+				putchar('');
+			}
+
+		}
+
+	}
+
+	putchar('\n');
+
+	return(0);
+
+}
