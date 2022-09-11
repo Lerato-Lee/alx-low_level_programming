3900389688597c67b6f989cffa77d01ba40f7db0
Lerato-Lee <mthimkhululerato72@gmail.com>
2022-09-11 05:57:02 -0700

    print base

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -12,21 +12,25 @@ int main(void)
 	for (i = 0; i <= 9; i++)
 
 	{
-		if (j > i)
+		for (j = 1; j <= 9; j++)
 
 		{
 			putchar(i + '0');
-			putchar(j + '0');
-			if (i != 8)
 
+			putchar(j + '0');
+
+			if (i != 8)
+			
 			{
 				putchar(',');
 				putchar(' ');
+
 			}
+
 		}
+
 	}
 
 	putchar('\n');
-
 	return (0);
 }
