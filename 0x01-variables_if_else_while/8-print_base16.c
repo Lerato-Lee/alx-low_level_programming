f8e1d5333df422488862438c65ba5d691559f349
Lerato-Lee <mthimkhululerato72@gmail.com>
2022-09-11 05:21:30 -0700

    prints base

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,27 +1,24 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - prints a hexadecimal string
  *
  * Return: 0 (Success)
  */
 int main(void)
 {
-
 	char c;
 
-	for (c = '0'; c <= '9'; c++)
+	for  (c = '0'; c <= '9'; c++)
 	{
 		putchar(c);
-	
 	}
 
 	for (c = 'a'; c <= 'f'; c++)
 	{
 		putchar(c);
-
 	}
 
-	putchar('\n')
+	putchar('\n');
 
 	return (0);
 }
