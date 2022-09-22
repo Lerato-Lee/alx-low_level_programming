f84ad647647ce7ed263dbcdd992122bd542d5319
Lerato-Lee <mthimkhululerato72@gmail.com>
2022-09-22 14:57:44 -0700

    MAGIC

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,5 +19,6 @@ char *rot13(char *);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
 void print_buffer(char *b, int size);
 void print_number(int n);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
 
-#endif /*MAIN_H*/
+#endif 
